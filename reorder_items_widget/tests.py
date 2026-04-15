ce5cf8666b68561007c4218f28c2da949714fe2f
Thomas Leichtfuß <leichtfuss@systopia.de>
2026-04-15 14:03:07 +0200

    improved widget layout once more

--- reorder_items_widget/tests.py
+++ reorder_items_widget/tests.py
@@ -15,7 +15,7 @@ class WidgetTestCase(TestCase):
         widget = ReorderItemsWidget()
         html = widget.render('index', 1)
         input = '<input type="number" name="index" value="1" class="reorder-items-widget-index hidden">'
-        drag_handle = '<div title="Drag me!" class="drag-handle">⬍</div>'
+        drag_handle = '<div title="Drag me!" class="drag-handle">⇳</div>'
         self.assertInHTML(input, html)
         self.assertInHTML(drag_handle, html)
 
@@ -23,6 +23,6 @@ class WidgetTestCase(TestCase):
         widget = ReorderItemsWidget()
         html = widget.render('index', None)
         input = '<input type="number" name="index" class="reorder-items-widget-index">'
-        drag_handle = '<div title="Drag me!" class="drag-handle">⬍</div>'
+        drag_handle = '<div title="Drag me!" class="drag-handle">⇳</div>'
         self.assertInHTML(input, html)
         self.assertNotIn(drag_handle, html)
