bdfae30427c64bb8b7782e0a0529f9d6a7265e21
Thomas Leichtfuß <leichtfuss@systopia.de>
2026-04-15 10:13:16 +0200

    added super simple test case to check python code

--- reorder_items_widget/tests.py
+++ reorder_items_widget/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from .widgets import ReorderItemsWidget
 
-# Create your tests here.
+# Just for coverage score.
+from . import __version__
+
+
+class WidgetTestCase(TestCase):
+    def test_widget(self):
+        widget = ReorderItemsWidget()
+        css_classes = widget.build_attrs(dict(), dict())['class']
+        self.assertIn('hidden', css_classes)
+        self.assertIn('reorder-items-widget-index', css_classes)
